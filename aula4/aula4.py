722e631df259868969dd7987de97166e30278840
Pedro Miranda <pedrocadaluz@gmail.com>
2025-08-26 09:12:34 -0300

    fix

--- aula4/aula4.py
+++ aula4/aula4.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 
 uri = 'https://api.football-data.org/v4/matches'
-headers = { 'X-Auth-Token': 'fdb570a7282c4dceac2fbb71a7104c05' }
+headers = { 'X-Auth-Token': 'token' }
 response = requests.get(uri, headers=headers)
 response = response.json()
 matches = response["matches"]
